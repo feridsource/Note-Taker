c0c2d48bdcfc9f7e8b61178bd0d4e5ac01cfbf81
feridsource <ferid.developer@gmail.com>
2015-09-08 10:03:41 +0300

    v3.1.0
    Speech recognition was added

--- app/src/main/java/com/ferid/app/notetake/MainActivity.java
+++ app/src/main/java/com/ferid/app/notetake/MainActivity.java
@@ -18,24 +18,35 @@ package com.ferid.app.notetake;
 
 import android.app.AlertDialog;
 import android.appwidget.AppWidgetManager;
+import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ferid.app.notetake.prefs.PrefsUtil;
 import com.ferid.app.notetake.widget.NoteWidget;
 
+import java.util.ArrayList;
+
+/**
+ * @author Ferid Cafer
+ */
 public class MainActivity extends AppCompatActivity {
     private Context context;
     private EditText notePad;
+
+    private final int SPEECH_REQUEST_CODE = 0;
 
     /**
      * Called when the activity is first created.
@@ -63,13 +74,39 @@ public class MainActivity extends AppCompatActivity {
         handler.post(new Runnable() {
             @Override
             public void run() {
-                final String note = PrefsUtil.getInstance(context).getNote();
-                if (notePad != null) {
-                    notePad.setText(note);
-                    notePad.setSelection(notePad.getText().length());
-                }
+                String note = PrefsUtil.getInstance(context).getNote();
+
+                writeIntoNote(note);
             }
         });
+    }
+
+    /**
+     * Write a text into notePad Edittext
+     * @param note
+     */
+    private void writeIntoNote(final String note) {
+        if (notePad != null) {
+            notePad.setText(note);
+            notePad.setSelection(notePad.getText().length());
+        }
+    }
+
+    /**
+     * Append note
+     * @param note
+     */
+    private void appendNote(final String note) {
+        if (notePad != null) {
+            //get the current note which was already written
+            String currentNote = notePad.getText().toString();
+            //if there is something written before, go to a new line
+            if (!currentNote.equals("")) {
+                currentNote += "\n";
+            }
+            //now write
+            writeIntoNote(currentNote + note);
+        }
     }
 
     /**
@@ -115,6 +152,33 @@ public class MainActivity extends AppCompatActivity {
         startActivity(Intent.createChooser(intent, getString(R.string.share)));
     }
 
+    /**
+     * Voice listener
+     */
+    private void listenToUser() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        try {
+            startActivityForResult(intent, SPEECH_REQUEST_CODE);
+        } catch (ActivityNotFoundException e) {
+            Toast toast = Toast.makeText(context,
+                    getString(R.string.voiceRecognitionNotSupported),
+                    Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        }
+    }
+
+    /**
+     * Capitalize the first letter of a given text
+     * @param text
+     * @return
+     */
+    private String capitalizeFirstLetter(String text) {
+        return text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
+    }
+
     @Override
     public void onBackPressed() {
         saveText();
@@ -151,14 +215,35 @@ public class MainActivity extends AppCompatActivity {
                 saveText();
                 finish();
                 return true;
+            case R.id.item_delete:
+                eraseAll();
+                return true;
             case R.id.item_share:
                 shareNotes();
                 return true;
-            case R.id.item_delete:
-                eraseAll();
+            case R.id.item_listen:
+                listenToUser();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            ArrayList<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+
+            //capitalize the first letter
+            spokenText = capitalizeFirstLetter(spokenText);
+            //now append the spoken text
+            appendNote(spokenText);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
